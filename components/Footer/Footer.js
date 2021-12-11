a395eff7ce06d4038e6b9a32104e3d0b5378557d
Tanmaykk <tanmaykachroo@gmail.com>
2021-12-11 23:46:37 +0530

    flex-wrap-reverse footer

--- components/Footer/Footer.js
+++ components/Footer/Footer.js
@@ -3,7 +3,7 @@ import { BsGithub } from "react-icons/bs";
 
 const Footer = () => {
   return (
-    <footer className="mx-auto w-10/12 border-t border-gray-600 mt-16 flex justify-between p-7 text-gray-400 font-medium text-lg">
+    <footer className="mx-auto w-10/12 border-t border-gray-600 mt-16 flex flex-wrap-reverse justify-between p-7 text-gray-400 font-medium text-lg">
       <div>Group 16</div>
       <div className="flex gap-x-7 flex-wrap ">
         <Link href="/">
