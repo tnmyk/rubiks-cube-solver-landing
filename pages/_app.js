aa36b57889f8366bce7dfe7ca921ee7b68705d47
Tanmaykk <tanmaykachroo@gmail.com>
2021-12-11 23:54:33 +0530

    Added head

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,13 @@
+import Head from "next/head";
 import Layout from "../components/Layout/Layout";
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
   return (
     <Layout>
+      <Head>
+        <title>Rubik's Cube Solver</title>
+      </Head>
       <Component {...pageProps} />
     </Layout>
   );
