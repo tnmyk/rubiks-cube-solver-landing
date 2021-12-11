a37990941f31ecdf30005cd70615181b1b3fd09a
Tanmaykk <tanmaykachroo@gmail.com>
2021-12-12 00:36:04 +0530

    Added favicon

--- pages/_app.js
+++ pages/_app.js
@@ -7,6 +7,24 @@ function MyApp({ Component, pageProps }) {
     <Layout>
       <Head>
         <title>Rubik's Cube Solver</title>
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/images/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/images/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/images/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
       </Head>
       <Component {...pageProps} />
     </Layout>
