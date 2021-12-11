fa4fc15e946d1a313d777eca5b22800e3d913240
Tanmaykk <tanmaykachroo@gmail.com>
2021-12-12 02:49:50 +0530

    Added Kociemba algo to tech

--- components/Home/Tech/techData.js
+++ components/Home/Tech/techData.js
@@ -1,5 +1,6 @@
 import { SiOpencv, SiPython } from "react-icons/si";
 import { GrReactjs } from "react-icons/gr";
+import { HiOutlineCube } from "react-icons/hi";
 const techData = [
   {
     logo: <SiOpencv />,
@@ -14,6 +15,11 @@ const techData = [
   {
     logo: <GrReactjs />,
     title: `React.js`,
+    desc: `Something about this tech Something about this tech Something about this tech Something about this tech Something about this tech Something about this tech`,
+  },
+  {
+    logo: <HiOutlineCube />,
+    title: `Kociemba's Algorithm`,
     desc: `Something about this tech Something about this tech Something about this tech Something about this tech Something about this tech Something about this tech`,
   },
 ];
