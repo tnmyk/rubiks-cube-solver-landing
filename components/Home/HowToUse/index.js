5003643eed1f9d4cbe6fa6140e23102fc9672141
Tanmaykk <tanmaykachroo@gmail.com>
2021-12-12 00:01:55 +0530

    Fixed yt embedd error

--- components/Home/HowToUse/index.js
+++ components/Home/HowToUse/index.js
@@ -2,16 +2,18 @@ const HowToUse = () => {
   return (
     <>
       <div className="">
-        <h1 className="text-3xl font-medium mt-24 text-gray-300 mb-5">How to use?</h1>
+        <h1 className="text-3xl font-medium mt-24 text-gray-300 mb-5">
+          How to use?
+        </h1>
       </div>
       <iframe
         width="560"
         height="315"
         src="https://www.youtube.com/embed/5DCo_bZ0PEo"
         title="YouTube video player"
-        frameborder="0"
+        frameBorder="0"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-        allowfullscreen
+        allowFullScreen
       ></iframe>
     </>
   );
