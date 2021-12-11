02aa95030290b6ac6a7a3de75bf4138e758b6823
Tanmaykk <tanmaykachroo@gmail.com>
2021-12-11 19:26:53 +0530

    github logo changes

--- components/Footer/Footer.js
+++ components/Footer/Footer.js
@@ -1,11 +1,16 @@
 import Link from "next/link";
+import { BsGithub } from "react-icons/bs";
 
 const Footer = () => {
   return (
     <footer className="mx-auto w-10/12 border-t border-gray-600 mt-16 flex justify-between p-7 text-gray-400 font-medium text-lg">
       <div>Group 16</div>
-      <div className="flex gap-x-5 ">
-        <Link href="/">Github</Link>
+      <div className="flex gap-x-7">
+        <Link href="/">
+          <a className="flex items-center gap-x-1">
+            Github <BsGithub />
+          </a>
+        </Link>
         <Link href="/">About</Link>
         <Link href="/">Members</Link>
         <Link href="/">About</Link>
