987fa6a6565e669bcb1f106de712a73ca046e3b8
Tanmaykk <tanmaykachroo@gmail.com>
2021-12-12 02:55:54 +0530

    Added svg to Intro Heading

--- pages/index.js
+++ pages/index.js
@@ -6,10 +6,13 @@ import Team from "../components/Home/Team";
 import Footer from "../components/Footer/Footer";
 import Features from "../components/Home/Features";
 import HowToUse from "../components/Home/HowToUse";
+import { BiCubeAlt } from "react-icons/bi";
 export default function Home() {
   return (
     <div className="flex flex-col items-center px-8">
-      <h1 className="font-bold text-8xl mt-32">Rubik's Cube Solver</h1>
+      <h1 className="font-bold text-7xl mt-32 flex gap-x-2 ">
+        Rubik's Cube Solver <BiCubeAlt style={{fontSize:'5rem'}}/>
+      </h1>
       <h3 className="text-2xl mt-5 text-gray-400">Made by group x members</h3>
       <div className="flex flex-wrap justify-center mt-10 gap-x-3 gap-y-2">
         <button className="p-3 w-56 font-medium text-lg rounded bg-green-500 flex justify-center items-center gap-x-2">
