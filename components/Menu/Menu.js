78febc02c09a5a338af0f93da0dbca5604ca99d9
Tanmaykk <tanmaykachroo@gmail.com>
2021-12-12 21:28:16 +0530

    menu changes

--- components/Menu/Menu.js
+++ components/Menu/Menu.js
@@ -1,5 +1,6 @@
 import Link from "next/link";
 import { useEffect } from "react";
+import { BiDownload } from "react-icons/bi";
 import styles from "./Menu.module.css";
 const Menu = ({ isMenuOpen }) => {
   useEffect(() => {
@@ -10,7 +11,12 @@ const Menu = ({ isMenuOpen }) => {
     <div className={`${styles.menu} ${isMenuOpen && styles.open}`}>
       {/* <div className="gap-x-10 text-gray-400 font-medium text-lg hidden md:flex"> */}
       <Link href="/">About</Link>
-      <Link href="/">Members</Link>
+      <Link href="/download">
+        <a className="flex items-center gap-x-2">
+          Download
+          <BiDownload className="text-2xl" />
+        </a>
+      </Link>
     </div>
   );
 };
