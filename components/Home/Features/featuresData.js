7095d53c2a28fc6ce60b6586a5d216bc905d6046
Tanmaykk <tanmaykachroo@gmail.com>
2021-12-12 15:45:57 +0530

    Added icons to Features

--- components/Home/Features/featuresData.js
+++ components/Home/Features/featuresData.js
@@ -1,18 +1,31 @@
+import { AiFillCamera } from "react-icons/ai";
+import { HiCursorClick } from "react-icons/hi";
+import { BsSpeedometer, BsGithub } from "react-icons/bs";
 const featuresData = [
   {
-    title: "Solve",
+    icon: <HiCursorClick />,
+    background: "bg-green-500",
+    title: "Easy UI",
     desc: `Something about solving Something about solving Something about solving Something about solving Something about solving Something about solving`,
   },
   {
-    title: "Solve",
+    icon: <AiFillCamera />,
+    background: "bg-red-500",
+    title: "Camera",
+    desc: `Something about solving Something about solving Something about solving Something about solving Something about solving Something about solving`,
+  },
+
+  {
+    title: "Fast",
+    icon: <BsSpeedometer />,
+    background: "bg-cyan-600",
     desc: `Something about solving Something about solving Something about solving Something about solving Something about solving Something about solving`,
   },
   {
-    title: "Solve",
-    desc: `Something about solving Something about solving Something about solving Something about solving Something about solving Something about solving`,
-  },
-  {
-    title: "Solve",
+    title: "Open-sourced",
+    icon: <BsGithub />,
+    background: "bg-white",
+    color: "text-black",
     desc: `Something about solving Something about solving Something about solving Something about solving Something about solving Something about solving`,
   },
 ];
--- components/Home/Features/index.js
+++ components/Home/Features/index.js
@@ -6,13 +6,18 @@ const Features = () => {
         <h1 className="text-3xl font-medium mt-24 text-gray-300">Features</h1>
       </div>
       <div className="flex gap-5 flex-wrap px-10 w-full justify-center mt-5">
-        {featuresData.map(({ title, desc }, index) => {
+        {featuresData.map(({ title, desc, icon, background, color }, index) => {
           return (
             <div
               key={index}
               style={{ minHeight: "15rem", minWidth: "17rem", width: "30%" }}
-              className="flex flex-col gap-y-5 items-center p-5 border rounded border-gray-500"
+              className="flex flex-col gap-y-2 items-center p-8 border rounded border-gray-500"
             >
+              <div
+                className={`text-5xl ${background} ${color} p-3 rounded-full mb-1`}
+              >
+                {icon}
+              </div>
               <div className="font-medium text-xl">{title}</div>
               <div className="w-full text-center font-medium text-gray-400">
                 {desc}
