a8e9cc627be17ab8c864a04eaeb751d7bd191f05
Tanmaykk <tanmaykachroo@gmail.com>
2021-12-12 15:54:27 +0530

    Added scroll to members

--- pages/index.js
+++ pages/index.js
@@ -7,14 +7,26 @@ import Footer from "../components/Footer/Footer";
 import Features from "../components/Home/Features";
 import HowToUse from "../components/Home/HowToUse";
 import { BiCubeAlt } from "react-icons/bi";
+import { useRef } from "react";
 export default function Home() {
+  const teamRef = useRef(null);
   return (
     <div className="flex flex-col items-center px-8">
       <h1 className="font-bold md:text-7xl mt-20 md:mt-32 text-5xl">
         Rubik's Cube Solver{" "}
         <BiCubeAlt style={{ display: "inline", fontSize: "5rem" }} />
       </h1>
-      <h3 className="text-2xl mt-5 text-gray-400">Made by group x members</h3>
+      <h3 className="text-2xl mt-5 text-gray-400">
+        Made by group x{" "}
+        <span
+          className="border-b-2 border-b-gray-400 cursor-pointer"
+          onClick={() => {
+            teamRef.current.scrollIntoView({ behavior: "smooth" });
+          }}
+        >
+          members
+        </span>
+      </h3>
       <div className="flex flex-wrap justify-center mt-10 gap-x-3 gap-y-2">
         <button className="p-3 w-56 font-medium text-lg rounded bg-green-500 flex justify-center items-center gap-x-2">
           View product <BsArrowRight />
@@ -26,6 +38,7 @@ export default function Home() {
       <Features />
       <HowToUse />
       <Tech />
+      <span ref={teamRef} style={{ scrollMarginTop: "-80px" }} />
       <Team />
     </div>
   );
