9e4a520e5dad9ee38cd548f4980e558f1dbe5b88
Tanmaykk <tanmaykachroo@gmail.com>
2021-12-20 21:49:50 +0530

    Added download link

--- pages/download.js
+++ pages/download.js
@@ -6,11 +6,17 @@ const Download = () => {
       <h1 className="font-medium text-5xl mt-28 mb-8 text-center">
         Download for Windows <AiFillWindows style={{ display: "inline" }} />
       </h1>
-      <button className="p-3 w-56 font-medium text-md rounded bg-green-500 flex justify-center items-center gap-x-2">
+
+      <a
+        href="https://api.github.com/repos/hacked-netizen/Rubiks-Cube-solver/zipball/main"
+        target="_blank"
+        rel="noreferrer"
+        className="p-3 w-56 font-medium text-md rounded bg-green-500 flex justify-center items-center gap-x-2"
+      >
         <FiDownload />
         Download
-      </button>
-      <span className="text-sm text-gray-400 mt-1">(70 Mb .exe)</span>
+      </a>
+      <span className="text-sm text-gray-400 mt-1">(compressed .zip)</span>
     </div>
   );
 };
