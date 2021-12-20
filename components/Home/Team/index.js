e5e060efbd1d2c12c79d18ea2b632cb891c9cc8f
Tanmaykk <tanmaykachroo@gmail.com>
2021-12-20 22:16:30 +0530

    Removed contri

--- components/Home/Team/index.js
+++ components/Home/Team/index.js
@@ -24,14 +24,13 @@ const Team = () => {
   return (
     <>
       <div className="">
-        <h1 className="text-3xl font-medium mt-24 text-gray-300">Group 160 team members</h1>
+        <h1 className="text-3xl font-medium mt-24 text-gray-300">
+          Group 160 team members
+        </h1>
       </div>
       <div className="flex gap-5 flex-wrap px-10 w-full justify-center mt-5">
         {teamData.map(
-          (
-            { name, rollNumber, contribution, photoURL, linkedIn, github },
-            index
-          ) => {
+          ({ name, rollNumber, photoURL, linkedIn, github }, index) => {
             return (
               <div
                 key={index}
@@ -53,9 +52,6 @@ const Team = () => {
                 <div className="flex flex-wrap justify-center gap-x-3">
                   <ProfileLink website={"linkedIn"} link={linkedIn} />
                   <ProfileLink website={"github"} link={github} />
-                </div>
-                <div className="w-full text-center font-medium text-gray-400 mt-3">
-                  {contribution}
                 </div>
               </div>
             );
--- components/Home/Team/teamData.js
+++ components/Home/Team/teamData.js
@@ -2,29 +2,24 @@ const teamData = [
   {
     name: "Pratham Sachdev",
     rollNumber: "20BCE10444",
-    contribution: "Worked on webpage.",
     github: "https://github.com/prath26",
   },
   {
     name: "Gaurav Tilwani",
     rollNumber: "20BCE10429",
-    contribution: "Worked on webpage.",
   },
   {
     name: "Nihar Goyal",
     rollNumber: "20BCE10936",
-    contribution: "Worked on webpage.",
   },
   {
     name: "Utkarsh Swarup",
     rollNumber: "20BCE10101​",
-    contribution: "Worked on webpage.",
     github: "https://github.com/hacked-netizen",
   },
   {
     name: "Tanmay Kachroo",
     rollNumber: "20BCE10142",
-    contribution: "Worked on webpage.",
     github: "https://github.com/tnmyk",
   },
 ];
